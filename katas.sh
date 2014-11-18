46d6689c13edf253195e920b30dd846100a60e5d
Yancy <podormeijia@gmail.com>
2014-11-18 20:50:30 +0800

    shell add: find.

--- katas.sh
+++ katas.sh
@@ -3,4 +3,7 @@ echo 'Text through stdin' | cat - has_content.txt
 cat -s multiple_blanks.txt
 cat multiple_blanks | tr -s '\n'
 
-
+#find
+find . \( -name 'file.txt' -o -name 'a.pdf' \)
+find . -path '*tmp*' #find . -path 'tmp' 
+find . -iregex ".*\.\(py\|pdf\)" # not work, why?
