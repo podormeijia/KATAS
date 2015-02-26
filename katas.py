dad77fb3b7d2a5a415dc82d337644e5513273063
Cheng Yang <cheng.yang@rea-group.com>
2015-02-26 08:27:38 +0800

    python: Tic-Tac-Toe Checker

--- katas.py
+++ katas.py
@@ -86,4 +86,47 @@ def factorial(n):
 
 factorial = lambda n: reduce(lambda a, b: a * b, range(1, n + 1), 1) if n >= 0 else None
 
+#Tic-Tac-Toe Checker
+def isSolved(board):
+  for i in range(0,3):
+    if board[i][0] == board[i][1] == board[i][2] != 0:
+      return board[i][0]
+    elif board[0][i] == board[1][i] == board[2][i] != 0:
+      return board[0][i]
+      
+  if board[0][0] == board[1][1] == board[2][2] != 0:
+    return board[0][0]
+  elif board[0][2] == board[1][1] == board[2][0] != 0:
+    return board[0][0]
+
+  elif 0 not in board[0] and 0 not in board[1] and 0 not in board[2]:
+    return 0
+  else:
+    return -1
+    
+def some(f, xs): 
+  return reduce(lambda x,y: x or f(y), xs, False)
+
+def every(f, xs): 
+  return reduce(lambda x,y: x and f(y), xs, True)
+
+def wins(player, line): 
+  return every(lambda x: x == player, line)
+def isSolved(board):
+  rows = board
+  cols = zip(*board)
+  diag = [board[i][i] for i in range(3)]
+  anti = [board[i][2-i] for i in range(3)]
+
+  lines = rows + cols + [diag, anti]
+
+  for i in (1,2):
+    if some(lambda line: wins(i, line), lines):
+      return i
+
+  if some(lambda line: some(lambda y: y == 0, line), lines):
+    return -1
+  else:
+    return 0
+
 
